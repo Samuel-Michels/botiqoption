2eff1da0898a1b278194142baa91e683f1fd9a91
Samuel-Michels <samuellmichells@gmail.com>
2021-01-28 14:22:14 -0300

    Aprimorando Login
    Aprimoramento login, sistema funcional

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from iqoptionapi.stable_api import IQ_Option
 
 #Importando demais bibliotecas
 from tkinter import *
+import getpass
 from datetime import datetime
 from dateutil import tz
 
@@ -11,5 +12,25 @@ from dateutil import tz
 login = ''
 password = ''
 
+def login():
+    log = str(input('Digite Seu login: ')).strip()
+    passwd = getpass.getpass('Digite sua senha: ')
+
+    print('Efetuando Login aguarde!')
+
+    return log, passwd
+
+
+login, password = login()
 
 iq = IQ_Option(login, password)
+
+check, reason = iq.connect()
+
+if check == True:
+    print('Bem-vindo!')
+    input('Pressione enter!')
+    
+else:
+    print('Erro no Login!')
+    
